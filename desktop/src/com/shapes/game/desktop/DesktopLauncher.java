b4cfafa89b8985b1420c5bc16321bec1586f90d1
cdgira <cdgira@cs.ship.edu>
2016-07-27 13:02:36 -0400

    Adding the core initial files to the repo.

---
+++ desktop/src/com/shapes/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shapes.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shapes.game.Shapes3dGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Shapes3dGame(), config);
+	}
+}
